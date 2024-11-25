c2f811009d7b3dc8664cad03ce4ba24ba2184243
RielleTatel <tatelgabrielle19@gmail.com>
2024-11-25 23:44:30 +0800

    3.00

---
+++ src/pages/VolunteerPages/VolunteerPage.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import MyNavbar from "../../components/NavigationBar/navbar";
+
+export const VolunteerPage = () => {
+  return (
+    <>
+      <MyNavbar />
+        <div className="sections">
+            <div className="content-container"> 
+
+            </div>
+
+            <div className="graphic-Container">     
+                <h1> </h1>
+            </div> 
+        </div>
+    </>
+  );
+};
+
+export default VolunteerPage;
