61970365750cdd7c0b5515213449a564ea442c5a
Qhaleed <139474360+Qhaleed@users.noreply.github.com>
2024-11-25 18:11:40 +0800

    Files upload

---
+++ BLUE-BRIDGE-FINAL-main/src/App.js
@@ -0,0 +1,23 @@
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+import { DonationPage } from './pages/DonationPages/DonationPage'
+
+import { PaymentPage } from './pages/PaymentPages/PaymentPage'
+import LandingPage from './pages/LandingPages/LandingPage';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+
+          <Route path="/page2" element={<DonationPage />} />
+          <Route path="/page3" element={<PaymentPage />} /> 
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
