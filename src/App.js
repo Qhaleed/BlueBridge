2e2edb9e285aae568a7c8c29241d5642f258f897
RielleTatel <tatelgabrielle19@gmail.com>
2024-11-25 23:42:09 +0800

    2.00

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import { DonationPage } from './pages/DonationPages/DonationPage'
-
+import { VolunteerPage } from './pages/VolunteerPages/VolunteerPage'
 import { PaymentPage } from './pages/PaymentPages/PaymentPage'
 import LandingPage from './pages/LandingPages/LandingPage';
 import './App.css';
@@ -12,6 +12,7 @@ function App() {
         <Routes>
           <Route path="/" element={<LandingPage />} />
           <Route path="/page2" element={<DonationPage />} />
+          <Route path="/page3" element={<VolunteerPage />} />
           <Route path="/page5" element={<PaymentPage />} /> 
         </Routes>
       </Router>
