b7d3a6ee96a0ee5a6d3f3270aa721a69491d6717
Qhaleed <reslykadiri@gmail.com>
2024-11-25 23:20:36 +0800

    section-1 updated

--- src/pages/LandingPages/LandingPage.js
+++ src/pages/LandingPages/LandingPage.js
@@ -10,6 +10,11 @@ export const LandingPage = () => {
       <MyNavbar />
       <div className='section1' id='home'>
         <img src='global/1.png'></img>
+        <div className='section-1-text-container'>
+          <h1 className='section-1-head'>What is BlueRidge?</h1>
+          <p>consectetur adipiscing elit, sed do <br />
+            eiusmod tempor  incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+        </div>
       </div>
       <BlueBorder />
 
